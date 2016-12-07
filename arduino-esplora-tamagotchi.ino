0cdc1c2c90116fd0c36ae595012167b17db9ec9b
Lucas Masotti <lucasmasotti@gmail.com>
2016-12-07 02:11:59 -0200

    Printing temperature perception. Displaying temperature perception using the RGB led

--- arduino-esplora-tamagotchi.ino
+++ arduino-esplora-tamagotchi.ino
@@ -12,6 +12,12 @@ struct Position {
   int y;
 };
 
+enum TemperaturePerception {
+  cold,
+  regular,
+  hot
+};
+
 int screenWidth;
 int screenHeight;
 
@@ -20,7 +26,7 @@ int environmentTemperature;
 
 const Position AR_CONDITIONER_TEMP_POS = {65, 5};
 int houseTemperature;
-const int AR_CONDITIONER_MIN_TEMP = 15;
+const int AR_CONDITIONER_MIN_TEMP = 5;
 const int AR_CONDITIONER_MAX_TEMP = 30;
 
 const Position LUMINOSITY_POS = {110, 5};
@@ -29,6 +35,8 @@ const RGB BACKGROUND_COLOR = { 0, 0, 0 };
 
 const Position STATUS_BASE_POS = {100, 30};
 const int STATUS_LINE_HEIGHT = 15;
+
+const int LUMINOSITY_DARK = 10;
 
 void setup() {
   EsploraTFT.begin();
@@ -44,19 +52,11 @@ void setup() {
   EsploraTFT.text("HUN", STATUS_BASE_POS.x, STATUS_BASE_POS.y + STATUS_LINE_HEIGHT);
   EsploraTFT.text("SLE", STATUS_BASE_POS.x, STATUS_BASE_POS.y + (STATUS_LINE_HEIGHT * 2) );
   EsploraTFT.text("STA", STATUS_BASE_POS.x, STATUS_BASE_POS.y + (STATUS_LINE_HEIGHT * 3) );
-  
-  EsploraTFT.line(0, 0, 0, 100);
 } 
 
 void loop() {  
   EsploraTFT.fill(255,255,255);
   EsploraTFT.circle(screenWidth/2 - 30, screenHeight/2, 10);
-
-  /*
-  EsploraTFT.noFill();
-  EsploraTFT.stroke(255, 255, 255);
-  EsploraTFT.rect(screenWidth/2, screenHeight/2, 80, 80);
-  */
 
   RGB white = { 255, 255, 255 };
   
@@ -67,7 +67,17 @@ void loop() {
   printTemperature(houseTemperature, AR_CONDITIONER_TEMP_POS, white);
 
   int luminosity = map(Esplora.readLightSensor(), 1023, 0, 100, 0);
-  printLuminosity(luminosity, LUMINOSITY_POS, white);    
+  printLuminosity(luminosity, LUMINOSITY_POS, white);
+
+  if(isDark(luminosity)) {
+    printSleepStatus(white);
+  } else {
+    clearSleepStatus();
+  }
+
+  TemperaturePerception temperaturePerception = getTemperaturePerception(environmentTemperature, houseTemperature);
+  printTemperaturePerception(temperaturePerception);
+  
   delay(1000);
   clearTemperature(environmentTemperature, ENVIRONMENT_TEMP_POS);
   clearTemperature(houseTemperature, AR_CONDITIONER_TEMP_POS);
@@ -97,3 +107,52 @@ void printTemperature(int temperature, Position pos, RGB color) {
 void clearTemperature(int temperature, Position pos) {
   printTemperature(temperature, pos, BACKGROUND_COLOR);
 }
+
+boolean isDark(int luminosity) {
+  return luminosity < LUMINOSITY_DARK;
+}
+
+void printSleepStatus(RGB color) {
+  EsploraTFT.stroke(color.r, color.g, color.b);
+  EsploraTFT.text("SLE", STATUS_BASE_POS.x + 20, STATUS_BASE_POS.y + (STATUS_LINE_HEIGHT * 3) );
+}
+void clearSleepStatus() {
+  printSleepStatus(BACKGROUND_COLOR);
+}
+
+TemperaturePerception getTemperaturePerception(int environmentTemperature, int houseTemperature) {
+  int mediumTemperature = (environmentTemperature + houseTemperature) / 2;
+  if(mediumTemperature < 16) {
+    return cold;
+  } else if(mediumTemperature >= 16 && mediumTemperature < 26) {
+    return regular;
+  } else {
+    return hot;
+  }
+}
+
+void printTemperaturePerception(TemperaturePerception temperature) {
+  char printout[4];
+  String temperatureText = String(temperature) + "C";
+  RGB color;
+  switch(temperature) {
+    case cold:
+      temperatureText = "COL";
+      Esplora.writeRGB(0, 0, 100);
+    break;
+    case regular:
+      temperatureText = "REG";
+      Esplora.writeRGB(0, 100, 0);
+    break;
+    case hot:
+      temperatureText = "HOT";
+      Esplora.writeRGB(100, 0, 0);
+    break;
+  }
+  temperatureText.toCharArray(printout, 4);
+  EsploraTFT.fill(BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b);
+  EsploraTFT.rect(STATUS_BASE_POS.x + 20, STATUS_BASE_POS.y + (STATUS_LINE_HEIGHT * 4), 20, 10);
+  EsploraTFT.stroke(255, 255, 255);
+  EsploraTFT.text(printout, STATUS_BASE_POS.x + 20, STATUS_BASE_POS.y + (STATUS_LINE_HEIGHT * 4) );
+}
+
